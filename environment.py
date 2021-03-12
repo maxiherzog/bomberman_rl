bfaf99677c0bd512e37a967c9c2bf3f152c40f37
maxiherzog <maxi.herzog@t-online.de>
2021-03-12 12:55:29 +0100

    Changed environment into only coins for task 1

--- environment.py
+++ environment.py
@@ -351,6 +351,7 @@ class BombeRLeWorld(GenericWorld):
                     if coin_pattern[i, j] == 1:
                         self.coins.append(Coin((x + i, x + j), self.arena[x+i,x+j] == 0))
                         coins[x + i, x + j] += 1"""
+
         for i in range(3):
             for j in range(3):
                 n_crates = (self.arena[1 + 5 * i:6 + 5 * i, 1 + 5 * j:6 + 5 * j] == 1).sum()
