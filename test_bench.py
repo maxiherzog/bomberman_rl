ccf05bc148d0cd9e306911faece9c748254bda5d
maxiherzog <maxi.herzog@t-online.de>
2021-03-27 15:58:22 +0100

    Test_results and meta analyis

--- test_bench.py
+++ test_bench.py
@@ -6,7 +6,7 @@ Created on Mon Mar 22 03:03:23 2021
 """
 
 import os
-
+import gc
 ROUNDS = 100
 # os.system('cd ../..')
 # os.system(f'python main.py play --agents task2_agent --no-gui --n-rounds {ROUNDS}')
@@ -14,17 +14,17 @@ import sys
 
 AGENT = "task2_agent"
 ALL_MODELS = True
-MODELS = ["HANS"]  # ,"HANS_SYM", "PHILIPP"]
+MODELS = []  # ,"HANS_SYM", "PHILIPP"]
 
 if ALL_MODELS:
     rootdir = f"agent_code/{AGENT}"
     for file in os.listdir(rootdir):
         if os.path.isdir(os.path.join(rootdir, file)):
-            if file.startswith("model_"):
+            if file.startswith("model_A"):
                 model = file[6:]
                 if not model.startswith("_") and model not in MODELS:
                     MODELS.append(model)
-sys.argv = [
+args = [
     "main.py",
     "play",
     "--agents",
@@ -33,12 +33,26 @@ sys.argv = [
     "--n-rounds",
     str(ROUNDS),
 ]
+sys.argv = args
+command_str = "python "
+for s in args:
+    command_str += s+" "
+print(command_str)
+
 
 for i in range(len(MODELS)):
     os.environ["TESTING"] = "YES"
     os.environ["MODELNAME"] = MODELS[i]
     print(MODELS[i])
-    exec(open("main.py").read())
+    res = os.system(command_str)
+    gc.collect()
+    # if res == 0:
+    #     rootdir = f"agent_code/{AGENT}/model"
+    #     # model ordner umbenennen!
+    #     os.rename(rootdir, f"agent_code/{AGENT}/model_A{alpha}-G{gamma}")
+    #     gc.collect()
+    # else:
+    #     raise
 
 os.environ["TESTING"] = "NO"
 os.environ["MODELNAME"] = ""
